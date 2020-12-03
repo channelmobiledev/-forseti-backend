b322533a719a074b63416864e6a83b855ee6f230
Channel Mobiledev <channel.mobiledev@gmail.com>
2020-12-03 19:04:13 +0000

    #2 - Add MVP Data

--- server.ts
+++ server.ts
@@ -1,12 +1,36 @@
 import express from 'express';
 
 const app = express();
-const PORT = 8000;
+const port = process.env.PORT || 8000;
 
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
-
-app;
-
-app.listen(PORT, () => {
-  console.log(`⚡[server]: Server is running at https://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`⚡[server]: Server is running at https://localhost:${port}`);
 });
+
+const controllerFeed = (req: any, res: any) => {
+  res.json({
+    data: [
+      {
+        user: {
+          name: 'Channel Mobile Dev',
+          avatar: 'https://source.unsplash.com/random/800x600'
+        },
+        post: {
+          title: 'Example overkill desk',
+          photo:
+            'https://cdn.shopify.com/s/files/1/0338/5360/3885/products/embody_prd_gallery_nevi_05_2x_0a3fc5a7-d667-4f5f-88a6-1b918ef56017_1280x1280.jpg'
+        }
+      }
+    ]
+  });
+};
+
+const handleErrors = (err: any) => {
+  if (err) {
+    return console.error(err);
+  }
+};
+
+app.get('/feed/', controllerFeed);
+app.on('error', handleErrors);
+app;
