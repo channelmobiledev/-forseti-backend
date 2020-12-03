cf9aaa3bf70490d0cad36dbdb13544d76ceea2cb
Channel Mobiledev <channel.mobiledev@gmail.com>
2020-12-03 19:35:54 +0000

    #2 - Add more dummy data

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-  console.log(`⚡[server]: Server is running at https://localhost:${port}`);
+  console.log(`⚡[server]: Server is running at http://localhost:${port}`);
 });
 
 const controllerFeed = (req: any, res: any) => {
@@ -20,9 +20,21 @@ const controllerFeed = (req: any, res: any) => {
           photo:
             'https://cdn.shopify.com/s/files/1/0338/5360/3885/products/embody_prd_gallery_nevi_05_2x_0a3fc5a7-d667-4f5f-88a6-1b918ef56017_1280x1280.jpg'
         }
+      },
+      {
+        user: {
+          name: 'Channel Mobile Dev',
+          avatar: 'https://source.unsplash.com/random/800x600'
+        },
+        post: {
+          title: 'Example overkill desk',
+          photo:
+            'https://cdn.shopify.com/s/files/1/0338/5360/3885/products/embody_prd_gallery_nevi_05_2x_0a3fc5a7-d667-4f5f-88a6-1b918ef56017_1280x1280.jpg'
+        }
       }
     ]
   });
+  console.log('GET /feed/ success');
 };
 
 const handleErrors = (err: any) => {
