8544189e8d699c6a5ba2bf86280be7d2dec721a9
Channel Mobiledev <channel.mobiledev@gmail.com>
2020-12-06 18:20:42 +0000

    #6 - Apply model to feed response

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import FeedItemModel from './models/FeedItemModel';
+import UserModel from './models/UserModel';
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -8,7 +10,7 @@ app.listen(port, () => {
 });
 
 const controllerFeed = (req: any, res: any) => {
-  res.json([
+  const response = new Array<FeedItemModel>(
     {
       user: {
         name: 'Channel Mobile Dev',
@@ -33,7 +35,10 @@ const controllerFeed = (req: any, res: any) => {
           'https://cdn.shopify.com/s/files/1/0338/5360/3885/products/embody_prd_gallery_nevi_05_2x_0a3fc5a7-d667-4f5f-88a6-1b918ef56017_1280x1280.jpg'
       }
     }
-  ]);
+  );
+
+  res.json(response);
+
   console.log('GET /feed/ success');
 };
 
