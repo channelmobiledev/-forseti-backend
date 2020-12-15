f652ad9a3c4d94b4fc2f2372622ea111377f098d
Channel Mobiledev <channel.mobiledev@gmail.com>
2020-12-15 18:14:05 +0000

    #13 - Remove extra dummy data

--- src/routes/RouteDesktop.ts
+++ src/routes/RouteDesktop.ts
@@ -33,69 +33,6 @@ const desktopGetDeskDetails = (req: any, res: any) => {
       type: 'Keyboard',
       name: 'Logitech K380',
       link: 'https://www.logitech.com/en-gb/product/multi-device-keyboard-k380'
-    },
-    {
-      icon: 'https://i.ebayimg.com/images/g/Q0gAAOSwq4dbgrZC/s-l1600.jpg',
-      type: 'Computer',
-      name: 'Asus N550JK',
-      link: 'http://buymenow.com'
-    },
-    {
-      icon:
-        'https://www.ikea.com/gb/en/images/products/linnmon-adils-table-black-brown-blue__0851744_PE594779_S5.JPG?f=g',
-      type: 'Desk',
-      name: 'Ikea LINNMON / ADILS',
-      link:
-        'https://www.ikea.com/gb/en/p/linnmon-adils-table-black-brown-blue-s29133599/'
-    },
-    {
-      icon:
-        'https://www.logitech.com/assets/65763/32/k380-multi-device-bluetooth-keyboard.jpg',
-      type: 'Keyboard',
-      name: 'Logitech K380',
-      link: 'https://www.logitech.com/en-gb/product/multi-device-keyboard-k380'
-    },
-    {
-      icon: 'https://i.ebayimg.com/images/g/Q0gAAOSwq4dbgrZC/s-l1600.jpg',
-      type: 'Computer',
-      name: 'Asus N550JK',
-      link: 'http://buymenow.com'
-    },
-    {
-      icon:
-        'https://www.ikea.com/gb/en/images/products/linnmon-adils-table-black-brown-blue__0851744_PE594779_S5.JPG?f=g',
-      type: 'Desk',
-      name: 'Ikea LINNMON / ADILS',
-      link:
-        'https://www.ikea.com/gb/en/p/linnmon-adils-table-black-brown-blue-s29133599/'
-    },
-    {
-      icon:
-        'https://www.logitech.com/assets/65763/32/k380-multi-device-bluetooth-keyboard.jpg',
-      type: 'Keyboard',
-      name: 'Logitech K380',
-      link: 'https://www.logitech.com/en-gb/product/multi-device-keyboard-k380'
-    },
-    {
-      icon: 'https://i.ebayimg.com/images/g/Q0gAAOSwq4dbgrZC/s-l1600.jpg',
-      type: 'Computer',
-      name: 'Asus N550JK',
-      link: 'http://buymenow.com'
-    },
-    {
-      icon:
-        'https://www.ikea.com/gb/en/images/products/linnmon-adils-table-black-brown-blue__0851744_PE594779_S5.JPG?f=g',
-      type: 'Desk',
-      name: 'Ikea LINNMON / ADILS',
-      link:
-        'https://www.ikea.com/gb/en/p/linnmon-adils-table-black-brown-blue-s29133599/'
-    },
-    {
-      icon:
-        'https://www.logitech.com/assets/65763/32/k380-multi-device-bluetooth-keyboard.jpg',
-      type: 'Keyboard',
-      name: 'Logitech K380',
-      link: 'https://www.logitech.com/en-gb/product/multi-device-keyboard-k380'
     }
   ];
 
