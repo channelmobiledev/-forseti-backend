7ceba7036c8ec19a72f61daed3061234ecd58b1d
Channel Mobiledev <channel.mobiledev@gmail.com>
2020-12-22 19:53:48 +0000

    #13 - Refactor db url

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,5 @@
 import { config } from 'process';
 
 export default class Config {
-  static db_url: string = 'mongodb://172.17.0.2/my_database';
-  // static db_url: string = 'mongodb://127.0.0.1/forseti-database';
+  static db_url: string = 'mongodb://172.17.0.2/forseti-database';
 }
