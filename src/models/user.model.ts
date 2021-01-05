0bb1202be826aeffcf299341dac96669f97f1413
Channel Mobiledev <channel.mobiledev@gmail.com>
2021-01-05 19:46:24 +0000

    #19 - Refactor user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,35 @@
+import { Document, Schema, model, Model } from 'mongoose';
+
+export interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string;
+  avatar: string;
+}
+
+const UserSchema: Schema = new Schema({
+  username: {
+    type: String,
+    minlength: [1, 'Field must not be empty'],
+    required: true
+  },
+  email: {
+    type: String,
+    minlength: [1, 'Field must not be empty'],
+    required: true
+  },
+  password: {
+    type: String,
+    minlength: [1, 'Field must not be empty'],
+    required: true
+  },
+  avatar: {
+    type: String,
+    minlength: [1, 'Field must not be empty'],
+    required: true
+  }
+});
+
+const User: Model<IUser> = model('users', UserSchema);
+
+export default User;
